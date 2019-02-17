e8908c82024430957966804c481702d3ca140309
Anita Virag <ik.virag@gmail.com>
2019-02-17 18:17:27 +0100

    type convert

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
-import math
+x = input("x: ")
+y = x + 1
 
-x = 10
+int(x)
+float(x)
+bool(x)
 
-x = 0b10
-print(bin(x))
-
-x = 0x12c
-print(hex(x))
-
-# a + bi
-x = 1 + 2j
-print(x)
-print(type(x))
-
-x = 10/3
-x = 10//3
-x = 10**3
-
-PI = 3.14
-print(round(PI))
-print(abs(PI))
-print(math.floor(PI))
+# Falsy
+# ""
+# 0
+# []
+# None (null)
