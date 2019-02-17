8561149a7e84f6025ec5d200e64ba866b06ec9a1
Anita Virag <ik.virag@gmail.com>
2019-02-17 17:43:37 +0100

    basic types

---
+++ app.py
@@ -0,0 +1,18 @@
+students_count = 1000
+print(type(students_count))
+
+rating = 4.99
+print(type(rating))
+
+rating = "good"
+print(type(rating))
+
+is_published = False
+print(type(is_published))
+
+course_name= """
+Multiple lines
+"""
+
+x, y = 1, 2
+x = y = 1
