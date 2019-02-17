1f326da6a403705e83add91e7c5eceeb3d2ae79b
Anita Virag <ik.virag@gmail.com>
2019-02-17 18:11:33 +0100

    number

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
-# \"
-# \'
-# \\
-# \n
+import math
 
-course = "Python Programming"
-len(course)
-course[0]
-course[-1]
-course[0:3]
-course[:3]
-course[0:]
-course[:]
+x = 10
 
-print(id(course))
-print(id(course[0]))
+x = 0b10
+print(bin(x))
 
-first = "John"
-last = "Smith"
-full = f"{first} {last}"
-print(full)
-test = f"{len(first)} {2 + 2}"
-print(test)
+x = 0x12c
+print(hex(x))
 
-print(course.upper())
-print(course.lower())
-print(course.title())
+# a + bi
+x = 1 + 2j
+print(x)
+print(type(x))
 
-print(course.find('Pro'))
-print(course.replace('P', '-'))
-print("Programming" in course)
-print("Programming" not in course)
+x = 10/3
+x = 10//3
+x = 10**3
+
+PI = 3.14
+print(round(PI))
+print(abs(PI))
+print(math.floor(PI))
