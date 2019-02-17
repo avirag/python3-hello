de139f04f9386ae476d81115025418acfee301e7
Anita Virag <ik.virag@gmail.com>
2019-02-17 17:56:26 +0100

    string

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-# immutable (bool, int, float, tuple, str, frozenset)
-x = 1
-print(id(x))
-x = x + 1
-print(id(x))
+# \"
+# \'
+# \\
+# \n
 
-# mutable (list, set, dict)
-y = [1, 2, 3]
-print(id(y))
-y.append(4)
-print(id(y))
+course = "Python Programming"
+len(course)
+course[0]
+course[-1]
+course[0:3]
+course[:3]
+course[0:]
+course[:]
+
+print(id(course))
+print(id(course[0]))
+
+first = "John"
+last = "Smith"
+full = f"{first} {last}"
+print(full)
+test = f"{len(first)} {2 + 2}"
+print(test)
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+print(course.find('Pro'))
+print(course.replace('P', '-'))
+print("Programming" in course)
+print("Programming" not in course)
